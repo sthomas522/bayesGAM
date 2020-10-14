dd590487f0cfb70e60857a31f7bd4912a305e436
Samuel Thomas <samthoma@iu.edu>
2020-10-14 11:26:13 -0400

    new data

--- R/data.r
+++ R/data.r
@@ -22,3 +22,26 @@
 #' @source Data provided by Wanzhu Tu, Indiana University School of Medicine
 #' @references Tu, W., Eckert, G. J., DiMeglio, L. A., Yu, Z., Jung, J., & Pratt, J. H. (2011). \emph{Intensified effect of adiposity on blood pressure in overweight and obese children}. Hypertension, 58(5), 818-824.
 "bloodpressure"
+
+#' Coral reef data from survey data on 6 sites
+#' 
+#' Data from 68 subjects
+#' 
+#' @format A data frame with 269 rows and 14 variables:
+#' \describe{
+#'   \item{ZONE}{Subject identification number}
+#'   \item{site}{Name of the habitat site}
+#'   \item{complexity}{habitat benthic complexity}
+#'   \item{rugosity}{a measurement related to terrain complexity}
+#'   \item{LC}{cover of low complexity}
+#'   \item{HC}{cover of high complexity}
+#'   \item{SCORE1}{PCA score 1 from Wilson, Graham, Polunin}
+#'   \item{SCORE2}{PCA score 2 from Wilson, Graham, Polunin}
+#'   \item{macro}{indicator of race white}
+#'   \item{species}{fish species}
+#'   \item{abundance}{fish abundance}
+#'   \item{biomass}{fish biomass}
+#' }
+#' @source Data from supplementary material provided for Fisher, R., Wilson, S. K., Sin, T. M., Lee, A. C., & Langlois, T. J. (2018). \emph{A simple function for full‐subsets multiple regression in ecology with R}. Ecology and evolution, 8(12), 6104-6113.
+#' @references Wilson, S. K., Graham, N. A. J., & Polunin, N. V. (2007). \emph{Appraisal of visual assessments of habitat complexity and benthic composition on coral reefs}. Marine Biology, 151(3), 1069-1076.
+"reef"
