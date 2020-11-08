8e2ef72278fa509d5f7acde2a8daeb947b050b23
Samuel Thomas <samthoma@iu.edu>
2020-11-07 21:08:11 -0500

    prep for cran

--- R/AllMethods.R
+++ R/AllMethods.R
@@ -1916,7 +1916,7 @@ setMethod("show", "bayesGAMfit",
             }
 
             default_pars <- stanobj@sim$pars_oi
-            pars <- default_pars[default_pars %in% c("beta", "eps", "lambda_rint", "lambda_reff", object@model@names_beta, "a")]
+            pars <- default_pars[default_pars %in% c("beta", "eps", "lambda", object@model@names_beta, "a")]
             print(x=stanobj, pars=pars)
           })
 
